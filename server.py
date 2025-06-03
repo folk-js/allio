4966f68087ccfc7df683265ed50fa15cf0277af1
Orion Reed <orionreedt@gmail.com>
2025-06-03 16:12:58 -0400

    wip

--- server.py
+++ server.py
@@ -1,17 +1,10 @@
-import http
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import Quartz
 import threading
 from flask_cors import CORS
-# import os
-# import time
-# import pyautogui
-# import pyperclip
 
 app = Flask(__name__)
 CORS(app)
-
-pointer = {"x": 0, "y": 0}
 
 
 def fetch_window_dims():
@@ -42,111 +35,19 @@ def fetch_window_dims():
     return visible_windows
 
 
-# def focus_plash():
-#     script = """
-#     tell application "Plash"
-#         activate
-#     end tell
-#     """
-#     os.system(f"osascript -e '{script}'")
-
-
 def success():
     return jsonify({"status": "success", "message": "we did it"})
 
 
-@app.route("/pointer", methods=["GET"])
-def get_pointer():
-    global pointer
-    return jsonify(pointer)
-
-
-@app.route("/pointer", methods=["POST"])
-def set_pointer():
-    data = request.json
-    # raise (data)
-    global pointer
-    x = data.get("x")
-    y = data.get("y")
-    if not x or not y:
-        raise ("missing mouse pos in json")
-
-    pointer = {"x": x, "y": y}
-    print(pointer)
-    return success()
+@app.route("/element", methods=["GET"])
+def get_elements():
+    global elements
+    return jsonify(elements)
 
 
 @app.route("/windows")
 def windows():
     return jsonify(fetch_window_dims())
-
-
-# @app.route("/write_text", methods=["POST"])
-# def write_text():
-#     data = request.json
-#     window_id = data.get("window_id")
-#     text = data.get("text")
-
-#     # Focus the target window
-#     focus_window(window_id)
-
-#     # Simulate typing the text
-#     pyautogui.write(text, interval=0.005)
-
-#     focus_plash()
-
-#     return success(window_id)
-
-
-# @app.route("/paste_text", methods=["POST"])
-# def paste_text():
-#     data = request.json
-#     window_id = data.get("window_id")
-#     text = data.get("text")
-
-#     pyperclip.copy(text)
-
-#     # Focus the target window
-#     focus_window(window_id)
-#     # focus_window(window_id)
-
-#     time.sleep(0.1)
-#     with pyautogui.hold(["command"]):
-#         time.sleep(0.1)
-#         pyautogui.press("v")
-#     # time.sleep(0.1)
-#     # pyautogui.write("hello world")
-#     # pyautogui.press("right")
-#     # time.sleep(0.1)
-#     # with pyautogui.hold(["command"]):
-#     #     time.sleep(0.1)
-#     #     pyautogui.press("v")
-#     # pyautogui.press("right")
-
-#     # focus_plash()
-
-#     return success(window_id)
-
-
-# def focus_window(window_id):
-#     # Fetch the list of windows and find the one with the matching kCGWindowNumber
-#     window_list = Quartz.CGWindowListCopyWindowInfo(
-#         Quartz.kCGWindowListOptionOnScreenOnly
-#         | Quartz.kCGWindowListExcludeDesktopElements,
-#         Quartz.kCGNullWindowID,
-#     )
-#     target_window = next(
-#         (window for window in window_list if window["kCGWindowNumber"] == window_id),
-#         None,
-#     )
-#     if target_window:
-#         pid = target_window["kCGWindowOwnerPID"]
-#         script = f"""
-#         tell application "System Events"
-#             set frontmost of the first process whose unix id is {pid} to true
-#         end tell
-#         """
-#         os.system(f"osascript -e '{script}'")
 
 
 if __name__ == "__main__":
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,32 @@
-import { register } from '@tauri-apps/api/globalShortcut';
-import { getCurrent } from '@tauri-apps/api/window';
-import { Tldraw } from 'tldraw'
+import { register } from "@tauri-apps/api/globalShortcut";
+import { getCurrent } from "@tauri-apps/api/window";
+import { Tldraw } from "tldraw";
 import "./App.css";
 
 const appWindow = getCurrent();
 let clickthrough = false;
-await register('CommandOrControl+Shift+E', () => {
-  console.log('Toggling clickthrough');
+await register("CommandOrControl+Shift+E", () => {
+  console.log("Toggling clickthrough");
   clickthrough = !clickthrough;
   appWindow.setIgnoreCursorEvents(clickthrough);
-  // if (clickthrough) {
-  //   appWindow.setFocus();
-  // }
 });
-
-window.addEventListener('mousemove', (ev) => {
-  const x = ev.screenX
-  const y = ev.screenY
-
-  fetch("http://127.0.0.1:5000/pointer", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ x, y })
-  })
-
-  // we can get pointer position...
-  // const result = fetch("http://127.0.0.1:5000/pointer").then(r => console.log(r.json()))
-
-})
 
 export default function App() {
   return (
-    <div style={{ position: 'fixed', inset: 0 }}>
+    <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw
-        persistenceKey='overlay'
+        persistenceKey="overlay"
         components={{
           MenuPanel: null,
           DebugPanel: null,
           Minimap: null,
           ZoomMenu: null,
-          HelpMenu: null
+          HelpMenu: null,
         }}
         cameraOptions={{
-          isLocked: true
+          isLocked: true,
         }}
-
       />
     </div>
-  )
+  );
 }
