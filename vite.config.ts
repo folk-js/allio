e860638931dbb18d5a3e6b2e89b2a986b4584099
Orion Reed <orionreedt@gmail.com>
2025-12-03 16:41:55 +0000

    fix deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -17,5 +15,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-  plugins: [wasm(), topLevelAwait()],
 }));
